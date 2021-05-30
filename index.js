ff4efdd17557e21175d62fcc32e773b363645287
Isela <isela.v.alarcon@gmail.com>
2021-05-30 17:31:20 +0200

    implement statics for react and scripts for heroku

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const path = require("path")
 const app = express()
 const server = http.Server(app)
 
+// Server static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
 // Listen Server
 const port = process.env.PORT || 5000
 server.listen(port, () => console.log(`server running on ${port}`))
---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+NPM_CONFIG_PRODUCTION=false
+NODE_MODULES_CACHE=false
+
+npm install --prefix client
+npm run build --prefix client
